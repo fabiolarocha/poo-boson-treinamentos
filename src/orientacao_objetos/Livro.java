579edf9e5438293d91c3e8407a45cf7d22440dfb
Fabiola Rocha <fabiolagrocha@outlook.com.br>
2022-06-23 20:35:28 -0300

    Iniciando projeto de POO - Bóson Treinamentos

---
+++ src/orientacao_objetos/Livro.java
@@ -0,0 +1,10 @@
+package orientacao_objetos;
+
+public class Livro {
+	String nome;
+	String descricao;
+	String autor;
+	String isbn;
+	double preco;
+	String dataPub;
+}
---
+++ src/orientacao_objetos/Principal.java
@@ -0,0 +1,18 @@
+package orientacao_objetos;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Livro livro = new Livro();
+		
+		livro.nome = "Bóson Linux";
+		livro.descricao = "Livro sobre Linux da Bóson!";
+		livro.isbn = "97867186617";
+		livro.preco = 45.95;
+		livro.autor = "Fábio Reis";
+		livro.dataPub = "20/04/2017";
+		
+		System.out.printf("A data de publicação do livro %s é %s", livro.nome, livro.dataPub);
+	}
+
+}
